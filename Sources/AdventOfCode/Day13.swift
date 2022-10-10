bf03e4c02ccf00ee52cd3b4437f19d29b7007f56
Oliver BINNS <oliver.binns@digital.cabinet-office.gov.uk>
2022-10-10 18:20:57 +0100

    Added Day 13 part 1

--- Sources/AdventOfCode/Day13.swift
+++ Sources/AdventOfCode/Day13.swift
@@ -1,15 +1,40 @@
 final class Day13: Solution {
     let day = 13
     
+    let earliestDeparture: Int
+    let buses: [Int?]
+    
+    
     init(input: String) {
-
+        let lines = input.split(whereSeparator: { $0.isWhitespace })
+        earliestDeparture = Int(lines[0])!
+        
+        buses = lines[1]
+            .split(separator: ",")
+            .map(String.init)
+            .map(Int.init)
     }
     
     func calculatePartOne() -> Int {
-        0
+        let timetable = buses
+            .compactMap { $0 }
+            .map { (busID: $0, nextDeparture: nextDeparture(ofBus: $0, after: earliestDeparture)) }
+        
+        guard let nextBus = timetable.min(by: { $0.nextDeparture < $1.nextDeparture }) else { return 0 }
+        
+        return nextBus.busID * (nextBus.nextDeparture - earliestDeparture)
+        
     }
     
     func calculatePartTwo() -> Int {
         0
+    }
+}
+
+extension Day13 {
+    func nextDeparture(ofBus busID: Int, after time: Int) -> Int {
+        // Integer division should take care of this for us!
+        let previousTime = (time / busID) * busID
+        return previousTime + busID
     }
 }
--- Tests/AdventOfCodeTests/Day13Tests.swift
+++ Tests/AdventOfCodeTests/Day13Tests.swift
@@ -5,10 +5,27 @@ final class Day13Tests: XCTestCase, SolutionTest {
     let day = 13
     
     func testPartOne() throws {
-        try XCTAssertEqual(Day14(input: getTestData()).calculatePartOne(), 0)
+        try XCTAssertEqual(Day13(input: getTestData()).calculatePartOne(), 295)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(Day14(input: getTestData()).calculatePartTwo(), 0)
+        try XCTAssertEqual(Day13(input: getTestData()).calculatePartTwo(), 0)
+    }
+}
+
+extension Day13Tests {
+    func testInitialisation() throws {
+        let sut = try Day13(input: getTestData())
+        XCTAssertEqual(sut.earliestDeparture, 939)
+        XCTAssertEqual(sut.buses, [7, 13, nil, nil, 59, nil, 31, 19])
+    }
+    
+    func testNextDepartureCalculation() throws {
+        let sut = try Day13(input: getTestData())
+        XCTAssertEqual(sut.nextDeparture(ofBus:  7, after: 929), 931)
+        XCTAssertEqual(sut.nextDeparture(ofBus: 13, after: 929), 936)
+        XCTAssertEqual(sut.nextDeparture(ofBus: 59, after: 929), 944)
+        XCTAssertEqual(sut.nextDeparture(ofBus: 31, after: 929), 930)
+        XCTAssertEqual(sut.nextDeparture(ofBus: 19, after: 929), 931)
     }
 }
