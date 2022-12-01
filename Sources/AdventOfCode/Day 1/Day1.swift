f57dd600538cc279a8884dc9eee37f07aa43d77f
Oliver BINNS <mail@oliverbinns.co.uk>
2022-12-01 06:56:21 +0000

    Completed Day 1

--- Sources/AdventOfCode/Day 1/Day1.swift
+++ Sources/AdventOfCode/Day 1/Day1.swift
@@ -1,17 +1,27 @@
 struct Day1: Solution {
     static let day = 1
     
+    let caloriesCarried: [Int]
+    
     init(input: String) {
-
+        caloriesCarried = input
+            .components(separatedBy: "\n\n")
+            .map {
+                $0
+                    .components(separatedBy: .newlines)
+                    .compactMap(Int.init)
+                    .reduce(0, +)
+            }
+            .sorted(by: >)
     }
 
     func calculatePartOne() -> Int {
-        0
+        caloriesCarried[0]
     }
 }
 
 extension Day1 {
     func calculatePartTwo() -> Int {
-        0
+        caloriesCarried[0...2].reduce(0, +)
     }
 }
--- Tests/AdventOfCodeTests/Day 1/Day1Tests.swift
+++ Tests/AdventOfCodeTests/Day 1/Day1Tests.swift
@@ -5,10 +5,24 @@ final class Day1Tests: XCTestCase, SolutionTest {
     typealias SUT = Day1
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 24000)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 45000)
+    }
+}
+
+extension Day1Tests {    
+    func testSum() throws {
+        func testTotals() throws {
+            try XCTAssertEqual(sut.caloriesCarried, [
+                24000,
+                11000,
+                10000,
+                6000,
+                4000
+            ])
+        }
     }
 }
