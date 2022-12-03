910e356e180eff397bc7da4b062045284f35579d
Oliver BINNS <mail@oliverbinns.co.uk>
2022-12-03 10:32:59 +0000

    Day 3 completed

--- Sources/AdventOfCode/Day 3/Day3.swift
+++ Sources/AdventOfCode/Day 3/Day3.swift
@@ -1,15 +1,68 @@
+import Algorithms
+
 struct Day3: Solution {
     static let day = 3
     
+    let rucksacks: [String]
+    
+    var intersections: [String] {
+        rucksacks
+            .map(compartments(in:))
+            .compactMap(intersection)
+    }
+    
     init(input: String) {
-        
+        rucksacks = input
+            .components(separatedBy: .newlines)
+            .filter { !$0.isEmpty }
     }
     
     func calculatePartOne() -> Int {
-        0
+        intersections
+            .map(priority)
+            .reduce(0, +)
+    }
+    
+    var badges: [String] {
+        groups(rucksacks: rucksacks)
+            .compactMap(intersection(_:))
     }
     
     func calculatePartTwo() -> Int {
-        0
+        badges
+            .map(priority)
+            .reduce(0, +)
+    }
+}
+
+extension Day3 {
+    func compartments(in rucksack: String) -> [Set<String>] {
+        let count = rucksack.count / 2
+        
+        let first = rucksack.prefix(count).map(String.init)
+        let last = rucksack.suffix(count).map(String.init)
+        
+        return [Set(first), Set(last)]
+    }
+    
+    func intersection(_ sets: [Set<String>]) -> String? {
+        sets.suffix(from: 1).reduce(sets[0]) { partialResult, newValue in
+            newValue.intersection(partialResult)
+        }.first
+    }
+    
+    func priority(of item: String) -> Int {
+        let asciiValue = item.compactMap(\.asciiValue)[0]
+        return Int(asciiValue) - (asciiValue > 96 ? 96 : 38)
+    }
+
+    func groups(rucksacks: [String]) -> [[Set<String>]] {
+        rucksacks
+            .chunks(ofCount: 3)
+            .map {
+                $0.map {
+                    Set($0.map(String.init))
+                }
+            }
     }
 }
--- Tests/AdventOfCodeTests/Day 3/Day3Tests.swift
+++ Tests/AdventOfCodeTests/Day 3/Day3Tests.swift
@@ -5,10 +5,57 @@ final class Day3Tests: XCTestCase, SolutionTest {
     typealias SUT = Day3
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 157)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 70)
+    }
+}
+
+extension Day3Tests {
+    func testInitialisation() throws {
+        try XCTAssertEqual(sut.rucksacks.count, 6)
+        try XCTAssertEqual(sut.rucksacks, [
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        ])
+    }
+    
+    func testCompartments() throws {
+        try XCTAssertEqual(sut.compartments(in: "vJrwpWtwJgWrhcsFMMfFFhFp"),
+                           [
+                            ["v", "J", "r", "w", "p", "W", "t", "J", "g"],
+                            ["h", "c", "s", "F", "M", "f", "h", "p"]
+                           ])
+    }
+    
+    func testIntersection() throws {
+        try XCTAssertEqual(sut.intersections,
+                           ["p", "L", "P", "v", "t", "s"])
+    }
+    
+    func testPriority() {
+        try XCTAssertEqual(sut.priority(of: "a"), 1)
+        try XCTAssertEqual(sut.priority(of: "b"), 2)
+        
+        try XCTAssertEqual(sut.priority(of: "z"), 26)
+        
+        try XCTAssertEqual(sut.priority(of: "A"), 27)
+        try XCTAssertEqual(sut.priority(of: "B"), 28)
+        
+        try XCTAssertEqual(sut.priority(of: "Z"), 52)
+    }
+    
+    func testGroups() {
+        let rucksacks = ["aab", "bbc", "ccd", "dde", "eef", "ffg"]
+        try XCTAssertEqual(sut.groups(rucksacks: rucksacks), [
+            [["a", "b"], ["b", "c"], ["c", "d"]],
+            [["d", "e"], ["e", "f"], ["f", "g"]]
+        ])
     }
 }
