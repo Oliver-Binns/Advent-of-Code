93bce6cfa02c0559c79bac26c72ae64ed2067eb1
Oliver BINNS <mail@oliverbinns.co.uk>
2022-12-04 10:24:50 +0000

    Completed Day 4

--- Sources/AdventOfCode/Day 4/Day4.swift
+++ Sources/AdventOfCode/Day 4/Day4.swift
@@ -1,15 +1,42 @@
 struct Day4: Solution {
     static let day = 4
+
+    let sections: [[ClosedRange<Int>]]
     
     init(input: String) {
-
+        sections = input
+            .components(separatedBy: .newlines)
+            .filter { !$0.isEmpty }
+            .compactMap {
+                $0
+                    .components(separatedBy: .punctuationCharacters)
+                    .compactMap(Int.init)
+                    .chunks(ofCount: 2)
+                    .compactMap { values in
+                        guard let first = values.first,
+                              let last = values.last else { return nil }
+                        return first...last
+                    }
+            }
     }
 
     func calculatePartOne() -> Int {
-        0
+        return sections.map {
+            $0[0].fullyContains($0[1]) ||
+            $0[1].fullyContains($0[0])
+        }.filter { $0 }.count
     }
     
     func calculatePartTwo() -> Int {
-        0
+        return sections.map {
+            $0[0].overlaps($0[1]) ||
+            $0[1].overlaps($0[0])
+        }.filter { $0 }.count
+    }
+}
+
+extension ClosedRange<Int> {
+    func fullyContains(_ range: ClosedRange<Int>) -> Bool {
+        range.clamped(to: self) == range
     }
 }
--- Tests/AdventOfCodeTests/Day 4/Day4Tests.swift
+++ Tests/AdventOfCodeTests/Day 4/Day4Tests.swift
@@ -5,10 +5,40 @@ final class Day4Tests: XCTestCase, SolutionTest {
     typealias SUT = Day4
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 2)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 4)
+    }
+}
+
+extension Day4Tests {
+    func testInitialisation() throws {
+        try XCTAssertEqual(sut.sections, [
+            [2...4, 6...8],
+            [2...3, 4...5],
+            [5...7, 7...9],
+            [2...8, 3...7],
+            [6...6, 4...6],
+            [2...6, 4...8]
+        ])
+    }
+    
+    func testFullyContains() {
+        XCTAssertFalse((2...3).fullyContains(4...5))
+        XCTAssertFalse((2...6).fullyContains(4...8))
+        XCTAssertTrue((2...8).fullyContains(3...7))
+        XCTAssertTrue((4...6).fullyContains(6...6))
+        
+        XCTAssertFalse((22...77).fullyContains(14...96))
+        XCTAssertTrue((14...96).fullyContains(22...77))
+    }
+    
+    func testMapping() throws  {
+        try XCTAssertEqual(sut.sections.map {
+            $0[0].fullyContains($0[1]) ||
+            $0[1].fullyContains($0[0])
+        }, [false, false, false, true, true, false])
     }
 }
