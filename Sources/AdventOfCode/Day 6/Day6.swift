7af7b4efb408f1380d79f7292d96bdddab0a9e11
Oliver BINNS <mail@oliverbinns.co.uk>
2022-12-06 12:19:55 +0000

    Completed Day 6

--- Sources/AdventOfCode/Day 6/Day6.swift
+++ Sources/AdventOfCode/Day 6/Day6.swift
@@ -1,15 +1,31 @@
+import Algorithms
+
 struct Day6: Solution {
     static let day = 6
     
+    let input: String
+    
     init(input: String) {
-        
+        self.input = input
     }
 
     func calculatePartOne() -> Int {
-        0
+       findMarker(ofLength: 4)
     }
     
     func calculatePartTwo() -> Int {
-        0
+        findMarker(ofLength: 14)
+    }
+    
+    func findMarker(ofLength length: Int) -> Int {
+        guard let index = input
+            .windows(ofCount: length)
+            .enumerated()
+            .first(where: { (index, value) in
+                Set(Array(value)).count == length
+            }) else {
+            preconditionFailure("No marker found")
+        }
+        return index.offset + length
     }
 }
--- Tests/AdventOfCodeTests/Day 6/Day6Tests.swift
+++ Tests/AdventOfCodeTests/Day 6/Day6Tests.swift
@@ -5,10 +5,34 @@ final class Day6Tests: XCTestCase, SolutionTest {
     typealias SUT = Day6
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 7)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 19)
+    }
+}
+
+extension Day6Tests {
+    func testAlternativesPartOne() {
+        XCTAssertEqual(SUT(input: "bvwbjplbgvbhsrlpgdmjqwftvncz")
+            .calculatePartOne(), 5)
+        XCTAssertEqual(SUT(input: "nppdvjthqldpwncqszvftbrmjlhg")
+            .calculatePartOne(), 6)
+        XCTAssertEqual(SUT(input: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+            .calculatePartOne(), 10)
+        XCTAssertEqual(SUT(input: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+            .calculatePartOne(), 11)
+    }
+    
+    func testAlternativesPartTwo() {
+        XCTAssertEqual(SUT(input: "bvwbjplbgvbhsrlpgdmjqwftvncz")
+            .calculatePartTwo(), 23)
+        XCTAssertEqual(SUT(input: "nppdvjthqldpwncqszvftbrmjlhg")
+            .calculatePartTwo(), 23)
+        XCTAssertEqual(SUT(input: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+            .calculatePartTwo(), 29)
+        XCTAssertEqual(SUT(input: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+            .calculatePartTwo(), 26)
     }
 }
