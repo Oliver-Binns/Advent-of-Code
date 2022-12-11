4f7b3a211269e670188c1a3b36ba105c1c9977c9
Oliver BINNS <mail@oliverbinns.co.uk>
2022-12-11 22:34:57 +0000

    Completed Day 11 | Part 1

--- Sources/AdventOfCode/Day 11/Day11.swift
+++ Sources/AdventOfCode/Day 11/Day11.swift
@@ -1,3 +1,5 @@
+import Algorithms
+
 struct Day11: Solution {
     static let day = 11
     
@@ -10,30 +12,109 @@ struct Day11: Solution {
     }
     
     func calculatePartOne() -> Int {
-        10605
+        (0..<20).reduce(monkeys) { partialResult, _ in
+            inspectItems(monkeys: partialResult)
+        }
+        .map(\.itemsInspected)
+        .max(count: 2)
+        .reduce(1, *)
     }
     
     func calculatePartTwo() -> Int {
         0
     }
+    
+    func inspectItems(monkeys: [Monkey]) -> [Monkey] {
+        var monkeys = monkeys
+        
+        for index in 0..<monkeys.count {
+            while !monkeys[index].items.isEmpty {
+                // multiply worry level:
+                let item = monkeys[index].inspectItem()
+                let worryLevel = monkeys[index].operation(item) / 3
+                let isDivisible = worryLevel % monkeys[index].test.divisibleBy == 0
+                let passToIndex = isDivisible ? monkeys[index].test.ifTrue : monkeys[index].test.ifFalse
+                
+                monkeys[passToIndex].items.append(worryLevel)
+            }
+        }
+        
+        return monkeys
+    }
 }
 
 struct Monkey {
-    let items: [Int]
+    var itemsInspected: Int = 0
+    var items: [Int]
     let operation: (Int) -> Int
     let test: Test
     
     init?(rawValue: String) {
-        return nil
+        let lines = rawValue.components(separatedBy: .newlines)
+        self.items = lines[1]
+            .components(separatedBy: [" ", ","])
+            .compactMap(Int.init)
+        
+        guard let operation = lines[2]
+            .components(separatedBy: "=")
+            .last?
+            .components(separatedBy: .whitespaces)
+            .filter(!\.isEmpty) else {
+            preconditionFailure("Invalid operation format")
+        }
+        
+        let op: (Int, Int) -> Int
+        switch operation[1] {
+        case "+": op = (+)
+        case "*": op = (*)
+        default: preconditionFailure("unexpected operation")
+        }
+        
+        self.operation = { value in
+            let lhs = Int(operation[0]) ?? value
+            let rhs = Int(operation[2]) ?? value
+            return op(lhs, rhs)
+        }
+        
+        test = .init(rawValue: Array(lines[3...]))
     }
     
-    func inspectItems() -> Monkey {
-        self
+    init(items: [Int],
+         operation: @escaping (Int) -> Int,
+         test: Test) {
+        self.items = items
+        self.operation = operation
+        self.test = test
+    }
+    
+    mutating func inspectItem() -> Int {
+        itemsInspected += 1
+        return items.removeFirst()
     }
 }
 
-struct Test {
+struct Test: Equatable {
     let divisibleBy: Int
     let ifTrue: Int
     let ifFalse: Int
+    
+    init(divisibleBy: Int,
+         ifTrue: Int,
+         ifFalse: Int) {
+        self.divisibleBy = divisibleBy
+        self.ifTrue = ifTrue
+        self.ifFalse = ifFalse
+    }
+    
+    init(rawValue: [String]) {
+        let values = rawValue.compactMap {
+            $0
+                .components(separatedBy: .whitespaces)
+                .compactMap(Int.init)
+                .first
+        }
+        self.divisibleBy = values[0]
+        self.ifTrue = values[1]
+        self.ifFalse = values[2]
+    }
 }
--- Tests/AdventOfCodeTests/Day 11/Day11Tests.swift
+++ Tests/AdventOfCodeTests/Day 11/Day11Tests.swift
@@ -15,6 +15,55 @@ final class Day11Tests: XCTestCase, SolutionTest {
 
 extension Day11Tests {
     func testInitialisation() throws {
-        try XCTAssertEqual(sut.monkeys.count, 4)
+        let monkeys = try sut.monkeys
+        XCTAssertEqual(monkeys.count, 4)
+        
+        XCTAssertEqual(monkeys[0].items, [79, 98])
+        XCTAssertEqual(monkeys[1].items, [54, 65, 75, 74])
+        XCTAssertEqual(monkeys[2].items, [79, 60, 97])
+        XCTAssertEqual(monkeys[3].items, [74])
+        
+        XCTAssertEqual(monkeys[0].operation(0), 0)
+        XCTAssertEqual(monkeys[0].operation(1), 19)
+        XCTAssertEqual(monkeys[0].operation(2), 38)
+        
+        XCTAssertEqual(monkeys[1].operation(0), 6)
+        XCTAssertEqual(monkeys[1].operation(1), 7)
+        XCTAssertEqual(monkeys[1].operation(2), 8)
+        
+        XCTAssertEqual(monkeys[2].operation(0), 0)
+        XCTAssertEqual(monkeys[2].operation(1), 1)
+        XCTAssertEqual(monkeys[2].operation(2), 4)
+        
+        XCTAssertEqual(monkeys[3].operation(0), 3)
+        XCTAssertEqual(monkeys[3].operation(1), 4)
+        XCTAssertEqual(monkeys[3].operation(2), 5)
+        
+        XCTAssertEqual(
+            monkeys[0].test,
+            .init(divisibleBy: 23, ifTrue: 2, ifFalse: 3)
+        )
+        XCTAssertEqual(
+            monkeys[1].test,
+            .init(divisibleBy: 19, ifTrue: 2, ifFalse: 0)
+        )
+        XCTAssertEqual(
+            monkeys[2].test,
+            .init(divisibleBy: 13, ifTrue: 1, ifFalse: 3)
+        )
+        XCTAssertEqual(
+            monkeys[3].test,
+            .init(divisibleBy: 17, ifTrue: 0, ifFalse: 1)
+        )
+    }
+    
+    func testInspectItems() throws {
+        let monkeys = try sut.inspectItems(monkeys: sut.monkeys)
+        
+        XCTAssertEqual(monkeys.count, 4)
+        XCTAssertEqual(monkeys[0].items, [20, 23, 27, 26])
+        XCTAssertEqual(monkeys[1].items, [2080, 25, 167, 207, 401, 1046])
+        XCTAssertTrue(monkeys[2].items.isEmpty)
+        XCTAssertTrue(monkeys[3].items.isEmpty)
     }
 }
