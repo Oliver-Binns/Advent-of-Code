0837342df9e32d015a4fccfcc923f8b2b4efb7b4
Oliver BINNS <mail@oliverbinns.co.uk>
2023-12-02 10:02:10 +0000

    Example of a bad refactor

--- Sources/AdventOfCode/Day 1/Day1.swift
+++ Sources/AdventOfCode/Day 1/Day1.swift
@@ -27,10 +27,9 @@ struct Day1: Solution {
     
     func runCalculation(isPartTwo: Bool = false) -> Int {
         lines.compactMap { line in
-            Int(
-                firstNumber(in: line, isPartTwo: isPartTwo) +
-                lastNumber(in: line, isPartTwo: isPartTwo)
-            )
+            Int(SearchDirection.allCases.map {
+                searchString(line, direction: $0, isPartTwo: isPartTwo)
+            }.joined())
         }.reduce(0, +)
     }
 
@@ -49,12 +48,16 @@ struct Day1: Solution {
 }
 
 extension Day1 {
-    func startsWith(string: String, isPartTwo: Bool = false) -> String? {
-        if string[string.startIndex].isNumber {
-            return String(string[string.startIndex])
+    func checkEndOfString(_ string: String,
+                          indexPath: KeyPath<String, String.Index>,
+                          find: (String, String) -> Bool,
+                          isPartTwo: Bool = false) -> String? {
+        let index = string[keyPath: indexPath]
+        if string[index].isNumber {
+            return String(string[index])
         } else if isPartTwo {
             for (index, number) in numbersToFind.enumerated()
-            where string.starts(with: number) {
+            where find(string, number) {
                 return index.description
             }
         }
@@ -62,38 +65,46 @@ extension Day1 {
         return nil
     }
     
-    func endsWith(string: String, isPartTwo: Bool = false) -> String? {
-        if string[string.lastIndex].isNumber {
-            return String(string[string.lastIndex])
-        } else if isPartTwo {
-            for (index, number) in numbersToFind.enumerated()
-            where string.hasSuffix(number) {
-                return index.description
+    func searchString(_ string: String, 
+                      direction: SearchDirection,
+                      isPartTwo: Bool = false) -> String {
+        for i in direction.iteration(over: string) {
+            let index = string.index(string.startIndex, offsetBy: i)
+            let substring = direction.substring(of: string, around: index)
+            if let number = checkEndOfString(String(substring),
+                                             indexPath: direction.indexPath,
+                                             find: direction.checkEnd,
+                                             isPartTwo: isPartTwo) {
+                return number
             }
+        }
+        preconditionFailure("Expected to find at least one number in string")
+    }
+    
+    enum SearchDirection: CaseIterable {
+        case forward
+        case backward
+        
+        var indexPath: KeyPath<String, String.Index> {
+            self == .forward ? \.startIndex : \.lastIndex
         }
         
-        return nil
-    }
-    
-    func firstNumber(in string: String, isPartTwo: Bool = false) -> String {
-        for i in 0..<string.count {
-            let index = string.index(string.startIndex, offsetBy: i)
-            let substring = String(string[index...])
-            if let number = startsWith(string: substring, isPartTwo: isPartTwo) {
-                return number
-            }
+        func iteration(over string: String) -> any Sequence<Int> {
+            self == .forward ?
+                0..<string.count :
+                (0..<string.count).reversed()
         }
-        preconditionFailure("Expected to find at least one number in string")
-    }
-    
-    func lastNumber(in string: String, isPartTwo: Bool = false) -> String {
-        for i in (0..<string.count).reversed() {
-            let index = string.index(string.startIndex, offsetBy: i)
-            let substring = String(string[...index])
-            if let number = endsWith(string: substring, isPartTwo: isPartTwo) {
-                return number
-            }
+        
+        func checkEnd(of string: String, for value: String) -> Bool {
+            self == .forward ?
+                string.hasPrefix(value) :
+                string.hasSuffix(value)
         }
-        preconditionFailure("Expected to find at least one number in string")
+        
+        func substring(of string: String, around index: String.Index) -> Substring {
+            self == .forward ?
+                string[index...] :
+                string[...index]
+        }
     }
 }
--- Tests/AdventOfCodeTests/Day 1/Day1Tests.swift
+++ Tests/AdventOfCodeTests/Day 1/Day1Tests.swift
@@ -26,46 +26,60 @@ final class Day1Tests: XCTestCase, SolutionTest {
 
 // MARK: - Part 1 Tests
 extension Day1Tests {
+    
     func testStartsWith() {
         try XCTAssertEqual(
-            sut.startsWith(string: "1abc2"),
+            sut.checkEndOfString("1abc2", 
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:)),
             "1"
         )
         try XCTAssertNil(
-            sut.startsWith(string: "pqr3stu8vwx")
+            sut.checkEndOfString("pqr3stu8vwx",
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:))
         )
         try XCTAssertEqual(
-            sut.startsWith(string: "3stu8vwx"),
+            sut.checkEndOfString("3stu8vwx",
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:)),
             "3"
         )
     }
     
     func testEndsWith() {
         try XCTAssertEqual(
-            sut.endsWith(string: "1abc2"),
+            sut.checkEndOfString("1abc2",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:)),
             "2"
         )
         try XCTAssertNil(
-            sut.endsWith(string: "pqr3stu8vwx")
+            sut.checkEndOfString("pqr3stu8vwx",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:))
         )
         try XCTAssertEqual(
-            sut.endsWith(string: "3stu8"),
+            sut.checkEndOfString("3stu8",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:),
+                                 isPartTwo: true),
             "8"
         )
     }
     
     func testFirstNumber() {
-        try XCTAssertEqual(sut.firstNumber(in: "1abc2"), "1")
-        try XCTAssertEqual(sut.firstNumber(in: "pqr3stu8vwx"), "3")
-        try XCTAssertEqual(sut.firstNumber(in: "a1b2c3d4e5f"), "1")
-        try XCTAssertEqual(sut.firstNumber(in: "treb7uchet"), "7")
+        try XCTAssertEqual(sut.searchString("1abc2", direction: .forward), "1")
+        try XCTAssertEqual(sut.searchString("pqr3stu8vwx", direction: .forward), "3")
+        try XCTAssertEqual(sut.searchString("a1b2c3d4e5f", direction: .forward), "1")
+        try XCTAssertEqual(sut.searchString("treb7uchet", direction: .forward), "7")
     }
     
     func testLastNumber() {
-        try XCTAssertEqual(sut.lastNumber(in: "1abc2"), "2")
-        try XCTAssertEqual(sut.lastNumber(in: "pqr3stu8vwx"), "8")
-        try XCTAssertEqual(sut.lastNumber(in: "a1b2c3d4e5f"), "5")
-        try XCTAssertEqual(sut.lastNumber(in: "treb7uchet"), "7")
+        try XCTAssertEqual(sut.searchString("1abc2", direction: .backward), "2")
+        try XCTAssertEqual(sut.searchString("pqr3stu8vwx", direction: .backward), "8")
+        try XCTAssertEqual(sut.searchString("a1b2c3d4e5f", direction: .backward), "5")
+        try XCTAssertEqual(sut.searchString("treb7uchet", direction: .backward), "7")
     }
     
     func testPartOne() throws {
@@ -77,77 +91,117 @@ extension Day1Tests {
 extension Day1Tests {
     func testStartsWithPartTwo() {
         try XCTAssertNil(
-            sut.startsWith(string: "two1nine")
+            sut.checkEndOfString("two1nine",
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:))
         )
         try XCTAssertEqual(
-            sut.startsWith(string: "two1nine", isPartTwo: true),
+            sut.checkEndOfString("two1nine",
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:),
+                                 isPartTwo: true),
             "2"
         )
         
         try XCTAssertNil(
-            sut.startsWith(string: "eightwothree")
+            sut.checkEndOfString("eightwothree",
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:))
         )
         try XCTAssertEqual(
-            sut.startsWith(string: "eightwothree", isPartTwo: true),
+            sut.checkEndOfString("eightwothree",
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:),
+                                 isPartTwo: true),
             "8"
         )
         
         try XCTAssertNil(
-            sut.startsWith(string: "abcone2threexyz")
+            sut.checkEndOfString("abcone2threexyz",
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:))
         )
         try XCTAssertNil(
-            sut.startsWith(string: "abcone2threexyz", isPartTwo: true)
+            sut.checkEndOfString("abcone2threexyz",
+                                 indexPath: \.startIndex,
+                                 find: string(_:hasPrefix:),
+                                 isPartTwo: true)
         )
     }
     
     func testFirstNumberPartTwo() {
         try XCTAssertEqual(
-            sut.firstNumber(in: "abcone2threexyz"),
+            sut.searchString("abcone2threexyz", direction: .forward),
             "2"
         )
         try XCTAssertEqual(
-            sut.firstNumber(in: "abcone2threexyz", isPartTwo: true),
+            sut.searchString("abcone2threexyz", direction: .forward, isPartTwo: true),
             "1"
         )
     }
     
     func testEndsWithPartTwo() {
         try XCTAssertNil(
-            sut.endsWith(string: "two1nine")
+            sut.checkEndOfString("two1nine",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:))
         )
         try XCTAssertEqual(
-            sut.endsWith(string: "two1nine", isPartTwo: true),
+            sut.checkEndOfString("two1nine",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:),
+                                 isPartTwo: true),
             "9"
         )
         
         try XCTAssertNil(
-            sut.endsWith(string: "eightwothree")
+            sut.checkEndOfString("eightwothree",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:))
         )
         try XCTAssertEqual(
-            sut.endsWith(string: "eightwothree", isPartTwo: true),
+            sut.checkEndOfString("eightwothree",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:),
+                                 isPartTwo: true),
             "3"
         )
         
         try XCTAssertNil(
-            sut.endsWith(string: "abcone2threexyz")
+            sut.checkEndOfString("abcone2threexyz",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:))
         )
         try XCTAssertNil(
-            sut.endsWith(string: "abcone2threexyz", isPartTwo: true)
+            sut.checkEndOfString("abcone2threexyz",
+                                 indexPath: \.lastIndex,
+                                 find: string(_:hasSuffix:),
+                                 isPartTwo: true)
         )
     }
     
     func testLastNumberPartTwo() {
         try XCTAssertEqual(
-            sut.lastNumber(in: "abcone2threexyz"),
+            sut.searchString("abcone2threexyz", direction: .backward),
             "2"
         )
         try XCTAssertEqual(
-            sut.lastNumber(in: "abcone2threexyz", isPartTwo: true),
+            sut.searchString("abcone2threexyz", direction: .backward, isPartTwo: true),
             "3"
         )
     }
     
     func testPartTwo() throws {
         try XCTAssertEqual(sut2.calculatePartTwo(), 281)
+    }
+}
+
+extension Day1Tests {
+    func string(_ string: String, hasPrefix prefix: String) -> Bool {
+        string.hasPrefix(prefix)
+    }
+    
+    func string(_ string: String, hasSuffix suffix: String) -> Bool {
+        string.hasSuffix(suffix)
     }
 }
