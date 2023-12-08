6aee11058a3d756df15d16a5b8985011c5d4e253
Oliver BINNS <mail@oliverbinns.co.uk>
2023-12-08 08:20:21 +0000

    Added Day 8 Part 1

--- Sources/AdventOfCode/Day 8/Day8.swift
+++ Sources/AdventOfCode/Day 8/Day8.swift
@@ -1,15 +1,70 @@
 struct Day8: Solution {
     static let day = 8
     
+    let instructions: [Instruction]
+    let map: [String: Node]
+    
     init(input: String) {
+        let parts = input.split(separator: "\n\n")
         
+        instructions = parts[0]
+            .compactMap(Instruction.init)
+        
+        map = parts[1]
+            .components(separatedBy: .newlines)
+            .filter { !$0.isEmpty }
+            .map(parseNode)
+            .reduce(into: [:]) {
+                $0[$1.0] = $1.1
+            }
     }
     
     func calculatePartOne() -> Int {
-        0
+        var currentLocation = "AAA"
+        var stepCount = 0
+        
+        while currentLocation != "ZZZ" {
+            for instruction in instructions {
+                stepCount += 1
+                
+                switch instruction {
+                case .left:
+                    currentLocation = map[currentLocation]!.left
+                case .right:
+                    currentLocation = map[currentLocation]!.right
+                }
+                
+                if currentLocation == "ZZZ" {
+                    return stepCount
+                }
+            }
+        }
+        
+        return stepCount
     }
     
     func calculatePartTwo() -> Int {
         0
     }
+}
+
+enum Instruction: Character {
+    case left = "L"
+    case right = "R"
+}
+
+struct Node: Equatable {
+    let left: String
+    let right: String
+}
+
+func parseNode(input: String) -> (String, Node) {
+    let data = Array(input.filter(\.isLetter))
+        .chunked(into: 3)
+        .map { String($0) }
+    
+    return (
+        data[0],
+        Node(left: data[1], right: data[2])
+    )
 }
--- Tests/AdventOfCodeTests/Day 8/Day8Tests.swift
+++ Tests/AdventOfCodeTests/Day 8/Day8Tests.swift
@@ -4,8 +4,27 @@ import XCTest
 final class Day8Tests: XCTestCase, SolutionTest {
     typealias SUT = Day8
     
+    func testInitialisation() {
+        try XCTAssertEqual(
+            sut.instructions,
+            [.right, .left]
+        )
+        try XCTAssertEqual(
+            sut.map, 
+            [
+                "AAA": Node(left: "BBB", right: "CCC"),
+                "BBB": Node(left: "DDD", right: "EEE"),
+                "CCC": Node(left: "ZZZ", right: "GGG"),
+                "DDD": Node(left: "DDD", right: "DDD"),
+                "EEE": Node(left: "EEE", right: "EEE"),
+                "GGG": Node(left: "GGG", right: "GGG"),
+                "ZZZ": Node(left: "ZZZ", right: "ZZZ"),
+            ]
+        )
+    }
+    
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 2)
     }
     
     func testPartTwo() throws {
