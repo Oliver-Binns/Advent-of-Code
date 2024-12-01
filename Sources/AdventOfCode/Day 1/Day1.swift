b0c26a5c3ea58f70556f8e3b9b0fa1894e8b5804
Oliver Binns <mail@oliverbinns.co.uk>
2024-12-01 09:48:22 +0000

    feat: implement day one solution (part 1 and 2)

--- Sources/AdventOfCode/Day 1/Day1.swift
+++ Sources/AdventOfCode/Day 1/Day1.swift
@@ -1,24 +1,54 @@
 struct Day1: Solution {
     static let day = 1
-    
+
+    let lists: ([Int], [Int])
+
     /// Initialise your solution
     ///
     /// - parameters:
     ///   - input: Contents of the `Day1.input` file within the same folder as this source file
     init(input: String) {
-
+        lists = input
+            .components(separatedBy: .newlines)
+            .map {
+                $0
+                .components(separatedBy: .whitespaces)
+                .compactMap(Int.init)
+            }
+            .reduce(into: ([], [])) { partialResult, line in
+                guard
+                    let first = line.first,
+                    let second = line.last else {
+                    return
+                }
+                partialResult.0.append(first)
+                partialResult.1.append(second)
+            }
     }
 
     /// Return your answer to the main activity of the advent calendar
     ///
     /// If you need to, you can change the return type of this method to any type that conforms to `CustomStringConvertible`, i.e. `String`, `Float`, etc.
     func calculatePartOne() -> Int {
-        0
+        let list1Sorted = lists.0.sorted()
+        let list2Sorted = lists.1.sorted()
+        return zip(list1Sorted, list2Sorted).map {
+            abs($0.0 - $0.1)
+        }.reduce(0, +)
     }
 
     /// Return your solution to the extension activity
     /// _ N.B. This is only unlocked when you have completed part one! _
     func calculatePartTwo() -> Int {
-        0
+        let leftListSorted = lists.0.sorted()
+
+        let rightListCount = Dictionary(
+            lists.1.map { ($0, 1) },
+            uniquingKeysWith: +
+        )
+
+        return leftListSorted.map {
+            $0 * (rightListCount[$0] ?? 0)
+        }.reduce(0, +)
     }
 }
--- Tests/AdventOfCodeTests/Day 1/Day1Tests.swift
+++ Tests/AdventOfCodeTests/Day 1/Day1Tests.swift
@@ -5,10 +5,24 @@ final class Day1Tests: XCTestCase, SolutionTest {
     typealias SUT = Day1
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 11)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 31)
+    }
+}
+
+extension Day1Tests {
+    func testParsing() {
+        try XCTAssertEqual(
+            sut.lists.0,
+            [3, 4, 2, 1, 3, 3]
+        )
+
+        try XCTAssertEqual(
+            sut.lists.1,
+            [4, 3, 5, 3, 9, 3]
+        )
     }
 }
