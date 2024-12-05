d7eab5bf2883da146dfa2077f0a774b026319b75
Oliver Binns <mail@oliverbinns.co.uk>
2024-12-05 09:24:59 +0000

    feat: add solutions for day 5 (part 1 and 2)

--- Sources/AdventOfCode/Day 5/Day5.swift
+++ Sources/AdventOfCode/Day 5/Day5.swift
@@ -1,15 +1,73 @@
 struct Day5: Solution {
     static let day = 5
-    
-    init(input: String) {
 
+    let dependencies: [Int: Set<Int>]
+    let updates: [[Int]]
+
+    init(input: String) {
+        let parts = input.split(separator: "\n\n")
+
+        dependencies = parts[0]
+            .components(separatedBy: .newlines)
+            .map {
+                $0.components(separatedBy: "|")
+                    .compactMap(Int.init)
+            }
+            .compactMap { array -> (Int, Int)? in
+                guard let first = array.first,
+                      let last = array.last else {
+                    return nil
+                }
+                return (first, last)
+            }
+            .reduce(into: [:]) { partialResult, dependency in
+                partialResult[
+                    dependency.0,
+                    default: []
+                ].insert(dependency.1)
+            }
+
+        updates = parts[1]
+            .components(separatedBy: .newlines)
+            .map {
+                $0.components(separatedBy: .punctuationCharacters)
+                    .compactMap(Int.init)
+            }
+            .filter { !$0.isEmpty }
     }
     
     func calculatePartOne() -> Int {
-        0
+        updates
+            .filter(isValid)
+            .map(middlePage)
+            .reduce(0, +)
     }
     
     func calculatePartTwo() -> Int {
-        0
+        updates
+            .filter { !isValid(update: $0) }
+            .map {
+                $0.sorted { lhs, rhs in
+                    dependencies[lhs]?.contains(rhs) ?? false
+                }
+            }
+            .map(middlePage)
+            .reduce(0, +)
     }
+
+    func isValid(update: [Int]) -> Bool {
+        update.sorted { lhs, rhs in
+            dependencies[lhs]?.contains(rhs) ?? false
+        } == update
+    }
+
+    func middlePage(of update: [Int]) -> Int {
+        let middleIndex = update.count / 2
+        return update[middleIndex]
+    }
+
 }
+
+
+
+
--- Tests/AdventOfCodeTests/Day 5/Day5Tests.swift
+++ Tests/AdventOfCodeTests/Day 5/Day5Tests.swift
@@ -5,10 +5,51 @@ final class Day5Tests: XCTestCase, SolutionTest {
     typealias SUT = Day5
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 143)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 123)
+    }
+}
+
+extension Day5Tests {
+    func testParsing() throws {
+        XCTAssertEqual(
+            try sut.updates,
+            [
+                [75,47,61,53,29],
+                [97,61,53,29,13],
+                [75,29,13],
+                [75,97,47,61,53],
+                [61,13,29],
+                [97,13,75,29,47]
+            ]
+        )
+    }
+
+    func testIsValid() throws {
+        XCTAssertTrue(try sut.isValid(update: [75,47,61,53,29]))
+        XCTAssertTrue(try sut.isValid(update: [97,61,53,29,13]))
+        XCTAssertTrue(try sut.isValid(update: [75,29,13]))
+
+        XCTAssertFalse(try sut.isValid(update: [75,97,47,61,53]))
+        XCTAssertFalse(try sut.isValid(update: [61,13,29]))
+        XCTAssertFalse(try sut.isValid(update: [97,13,75,29,47]))
+    }
+
+    func testMiddlePage() throws {
+        XCTAssertEqual(
+            try sut.middlePage(of: [75,47,61,53,29]),
+            61
+        )
+        XCTAssertEqual(
+            try sut.middlePage(of: [97,61,53,29,13]),
+            53
+        )
+        XCTAssertEqual(
+            try sut.middlePage(of: [75,29,13]),
+            29
+        )
     }
 }
