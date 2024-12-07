8b56615a822f0068114c8ee11ee57364d858f9ba
Oliver Binns <mail@oliverbinns.co.uk>
2024-12-07 11:50:17 +0000

    feat: add solutions for day 7 (part 1 and 2)

--- Sources/AdventOfCode/Day 7/Day7.swift
+++ Sources/AdventOfCode/Day 7/Day7.swift
@@ -1,15 +1,70 @@
 struct Day7: Solution {
     static let day = 7
-    
+
+    let equations: [Equation]
+
     init(input: String) {
-        
+        equations = input
+            .components(separatedBy: .newlines)
+            .compactMap(Equation.init)
     }
     
     func calculatePartOne() -> Int {
-        0
+        calculateAnswer(supportedOperators: [(*), (+)])
     }
     
     func calculatePartTwo() -> Int {
-        0
+        calculateAnswer(supportedOperators: [(*), (+), (||)])
     }
+
+    func calculateAnswer(
+        supportedOperators: [(Int, Int) -> Int]
+    ) -> Int {
+        equations
+            .filter { $0.canBeMet(supportedOperators: supportedOperators) }
+            .map(\.total)
+            .reduce(0, +)
+    }
+}
+
+extension Day7 {
+    struct Equation: Equatable {
+        let total: Int
+        let components: [Int]
+    }
+}
+
+extension Day7.Equation {
+    init?(_ input: String) {
+        let properties = input.split(separator: ":")
+        guard properties.count == 2,
+            let total = Int(properties[0]) else {
+            return nil
+        }
+        self.total = total
+        self.components = properties[1]
+            .components(separatedBy: .whitespaces)
+            .compactMap(Int.init)
+    }
+
+    func canBeMet(
+        supportedOperators: [(Int, Int) -> Int] = [(*), (+)]
+    ) -> Bool {
+        components.suffix(from: 1)
+            .reduce([components[0]]) { partialResult, nextValue in
+                partialResult.flatMap { value in
+                    supportedOperators.map { op in
+                        op(value, nextValue)
+                    }
+                }
+            }
+            .contains(total)
+    }
+}
+
+func || (lhs: Int, rhs: Int) -> Int {
+    guard let value = Int(String(lhs) + String(rhs)) else {
+        fatalError("Cannot join \(lhs) and \(rhs)")
+    }
+    return value
 }
--- Tests/AdventOfCodeTests/Day 7/Day7Tests.swift
+++ Tests/AdventOfCodeTests/Day 7/Day7Tests.swift
@@ -3,12 +3,57 @@ import XCTest
 
 final class Day7Tests: XCTestCase, SolutionTest {
     typealias SUT = Day7
-    
+
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 3749)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 11387)
     }
 }
+
+extension Day7Tests {
+    func testParsing() throws {
+        XCTAssertEqual(
+            try sut.equations,
+            [
+                .init(total: 190, components: [10, 19]),
+                .init(total: 3267, components: [81, 40, 27]),
+                .init(total: 83, components: [17, 5]),
+                .init(total: 156, components: [15, 6]),
+                .init(total: 7290, components: [6, 8, 6, 15]),
+                .init(total: 161011, components: [16, 10, 13]),
+                .init(total: 192, components: [17, 8, 14]),
+                .init(total: 21037, components: [9, 7, 18, 13]),
+                .init(total: 292, components: [11, 6, 16, 20]),
+            ]
+        )
+    }
+
+    func testCanBeMet() {
+        XCTAssertTrue(
+            Day7.Equation(
+                total: 190,
+                components: [10, 19]
+            ).canBeMet()
+        )
+        XCTAssertTrue(
+            Day7.Equation(
+                total: 3267,
+                components: [81, 40, 27]
+            ).canBeMet()
+        )
+        XCTAssertTrue(
+            Day7.Equation(
+                total: 292,
+                components: [11, 6, 16, 20]
+            ).canBeMet()
+        )
+    }
+
+    func testInfixOperator() {
+        XCTAssertEqual(12 || 345, 12345)
+    }
+}
+
