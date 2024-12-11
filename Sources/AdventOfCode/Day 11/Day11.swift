00289337a7b3e5624ac348a48bbe17323e03ce73
Oliver Binns <mail@oliverbinns.co.uk>
2024-12-11 23:41:59 +0000

    feat: add solutions for day 11 (part 1 and 2)

--- Sources/AdventOfCode/Day 11/Day11.swift
+++ Sources/AdventOfCode/Day 11/Day11.swift
@@ -1,15 +1,60 @@
 struct Day11: Solution {
     static let day = 11
     
+    let stones: [Int: Int]
+    
     init(input: String) {
-        
+        stones = input
+            .components(separatedBy: .whitespacesAndNewlines)
+            .compactMap(Int.init)
+            .reduce(into: [Int: Int]()) {
+                $0[$1] = 1
+            }
+    }
+    
+    func blink(stones: [Int: Int]) -> [Int: Int] {
+        stones.reduce(into: [:]) { (dictionary, element) in
+            let stone = element.key
+            let count = element.value
+            
+            switch stone {
+            case 0:
+                dictionary[1, default: 0] += count
+            case _ where stone.description.count.isEven:
+                let string = stone.description
+                let halfway = string.count / 2
+                guard let firstHalf = Int(string.prefix(halfway)),
+                      let secondHalf = Int(string.suffix(halfway))
+                else {
+                    break
+                }
+                dictionary[firstHalf, default: 0] += count
+                dictionary[secondHalf, default: 0] += count
+            default:
+                dictionary[stone * 2024, default: 0] += count
+            }
+            
+        }
+    }
+    
+    func blink(
+        times: Int,
+        stones: [Int: Int]
+    ) -> [Int: Int] {
+        (0..<times)
+            .reduce(stones) { stones, _ in
+                blink(stones: stones)
+            }
     }
     
     func calculatePartOne() -> Int {
-        0
+        blink(times: 25, stones: stones)
+            .values.reduce(0, +)
+        
     }
     
     func calculatePartTwo() -> Int {
-        0
+        blink(times: 75, stones: stones)
+            .values.reduce(0, +)
     }
 }
--- Tests/AdventOfCodeTests/Day 11/Day11Tests.swift
+++ Tests/AdventOfCodeTests/Day 11/Day11Tests.swift
@@ -5,10 +5,68 @@ final class Day11Tests: XCTestCase, SolutionTest {
     typealias SUT = Day11
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 55312)
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(sut.calculatePartTwo(), 65601038650482)
+    }
+}
+
+extension Day11Tests {
+    func testParsing() throws {
+        try XCTAssertEqual(
+            sut.stones,
+            [
+                125: 1,
+                17: 1
+            ]
+        )
+    }
+    
+    func testBlinkRule1() throws {
+        let blinkedStones = try sut.blink(stones: [0: 1])
+        // Rule 1:
+        // If the stone is engraved with the number 0,
+        // it is replaced by a stone engraved with the number 1.
+        XCTAssertEqual(blinkedStones[0, default: 0], 0)
+        XCTAssertEqual(blinkedStones[1, default: 0], 1)
+    }
+    
+    func testBlinkRule2() throws {
+        let blinkedStones = try sut.blink(stones: [10: 1, 99: 1])
+        // Rule 2:
+        // If the stone is engraved with a number that has an even
+        // number of digits, it is replaced by two stones.
+        //
+        // The left half of the digits are engraved on the new left
+        // stone, and the right half of the digits are engraved on
+        // the new right stone. (The new numbers don't keep extra
+        // leading zeroes: 1000 would become stones 10 and 0.)
+        XCTAssertEqual(blinkedStones[0], 1)
+        XCTAssertEqual(blinkedStones[1], 1)
+        XCTAssertEqual(blinkedStones[9], 2)
+    }
+    
+    func testBlinkRule3() throws {
+        let blinkedStones = try sut.blink(stones: [1: 1, 999: 1])
+        // Rule 3:
+        // If none of the other rules apply, the stone is replaced
+        // by a new stone; the old stone's number multiplied by 2024
+        // is engraved on the new stone.
+        XCTAssertEqual(blinkedStones[2024], 1)
+        XCTAssertEqual(blinkedStones[2021976], 1)
+    }
+    
+    func testSixBlinks() throws {
+        let blinkedStones = try sut.blink(
+            times: 6,
+            stones: [125: 1, 17: 1]
+        )
+        
+        XCTAssertEqual(
+            blinkedStones.values.reduce(0, +),
+            22
+        )
     }
 }
