e4f03b753b33f03e504660e838ab00c7b9e4714a
Oliver Binns <mail@oliverbinns.co.uk>
2024-12-14 12:34:51 +0000

    feat: add solutions for day 13, part 1

--- Sources/AdventOfCode/Day 13/Day13.swift
+++ Sources/AdventOfCode/Day 13/Day13.swift
@@ -1,16 +1,82 @@
 struct Day13: Solution {
     static let day = 13
-    
+
+    let machines: [ClawMachine]
+
     init(input: String) {
-        
+        machines = input
+            .components(separatedBy: "\n\n")
+            .filter { !$0.isEmpty }
+            .compactMap(ClawMachine.init)
     }
     
     func calculatePartOne() -> Int {
-        0
-        
+        machines
+            .map(winOptions)
+            .map { $0.map(+) }
+            .map { $0.min() ?? 0 }
+            .reduce(0, +)
+
     }
-    
+
+    func winOptions(for machine: ClawMachine) -> [(x: Int, y: Int)] {
+        var options: [(x: Int, y: Int)] = []
+
+        for a in (0..<100) {
+            for b in (0..<100) {
+
+                let x = a * machine.buttonA.x + b * machine.buttonB.x
+                let y = a * machine.buttonA.y + b * machine.buttonB.y
+
+                if x == machine.prize.x && y == machine.prize.y {
+                    options.append((x: a * 3, y: b))
+                } else if x > machine.prize.x || y > machine.prize.y {
+                    break
+                }
+            }
+        }
+
+        return options
+    }
+
     func calculatePartTwo() -> Int {
         0
     }
 }
+
+struct ClawMachine: Equatable {
+    let buttonA: Point
+    let buttonB: Point
+
+    let prize: Point
+}
+
+extension ClawMachine {
+    init?(string: String) {
+        let components = string.components(separatedBy: .newlines)
+
+        guard let buttonA = Self.parse(line: components[0]),
+              let buttonB = Self.parse(line: components[1]),
+              let prize = Self.parse(line: components[2]) else {
+            return nil
+        }
+
+        self.buttonA = buttonA
+        self.buttonB = buttonB
+        self.prize = prize
+    }
+
+    static func parse(line: String) -> Point? {
+        let search = /[A-Za-z ]+: X[\+=](?<x>\d+), Y[\+=](?<y>\d+)/
+        guard let result = try? search.wholeMatch(in: line),
+              let x = Int(result.x),
+              let y = Int(result.y) else {
+            print("no match")
+            return nil
+        }
+
+        return Point(x: x, y: y)
+    }
+}
+
+
--- Tests/AdventOfCodeTests/Day 13/Day13Tests.swift
+++ Tests/AdventOfCodeTests/Day 13/Day13Tests.swift
@@ -5,10 +5,45 @@ final class Day13Tests: XCTestCase, SolutionTest {
     typealias SUT = Day13
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 480)
     }
     
     func testPartTwo() throws {
         try XCTAssertEqual(sut.calculatePartTwo(), 0)
     }
 }
+
+extension Day13Tests {
+    func testParsing() throws {
+        XCTAssertEqual(
+            try sut.machines.count,
+            4
+        )
+        XCTAssertEqual(
+            try sut.machines[0],
+            ClawMachine(
+                buttonA: Point(x: 94, y: 34),
+                buttonB: Point(x: 22, y: 67),
+                prize: Point(x: 8400, y: 5400)
+            )
+        )
+    }
+
+    func testWinOptions() throws {
+        XCTAssertEqual(
+            try sut.winOptions(for: sut.machines[0]).count,
+            1
+        )
+        XCTAssertEqual(
+            try sut.winOptions(for: sut.machines[0])[0].x,
+            80 * 3
+        )
+        XCTAssertEqual(
+            try sut.winOptions(for: sut.machines[0])[0].y,
+            40
+        )
+    }
+}
+//Button A: X+94, Y+34
+//Button B: X+22, Y+67
+//Prize: X=8400, Y=5400
