932ad567441305a5dab4041d6de4195ae8f0ea9e
Oliver Binns <mail@oliverbinns.co.uk>
2024-12-15 10:46:49 +0000

    feat: add solutions for day 15, part 1

--- Sources/AdventOfCode/Day 15/Day15.swift
+++ Sources/AdventOfCode/Day 15/Day15.swift
@@ -1,15 +1,117 @@
 struct Day15: Solution {
     static let day = 15
-    
-    init(input: String) {
 
+    let map: Grid<FactoryContents>
+    let moves: [Move]
+
+    init(input: String) {
+        let components = input.components(separatedBy: "\n\n")
+        map = Grid(
+            string: components[0],
+            mapping: FactoryContents.init
+        )
+
+        moves = components[1].compactMap(Move.init)
     }
     
     func calculatePartOne() -> Int {
-        0
+        let finalState = makeMoves(moves, on: map)
+        return calculateGPSCoodinates(for: finalState)
     }
     
     func calculatePartTwo() -> Int {
         0
+    }
+
+    func makeMoves(
+        _ moves: [Move],
+        on grid: Grid<FactoryContents>
+    ) -> Grid<FactoryContents> {
+        moves.reduce(map) { makeMove($1, grid: $0) }
+    }
+
+    func makeMove(_ move: Move, grid: Grid<FactoryContents>) -> Grid<FactoryContents> {
+        guard let position = grid.findPositions(of: .robot).first else {
+            preconditionFailure("Could not find position of robot")
+        }
+
+        let direction = move.direction
+        var updatedMap = grid.values
+
+        let newPosition = position.move(in: direction)
+
+        switch updatedMap[newPosition.y][newPosition.x] {
+        case .wall:
+            // cannot move into wall
+            break
+        case .empty:
+            // move robot here!
+            updatedMap[newPosition.y][newPosition.x] = .robot
+            updatedMap[position.y][position.x] = .empty
+        case .robot:
+            preconditionFailure("there should be only one robot!")
+        case .box:
+            var boxPosition = newPosition
+
+            // if we find a wall, there is nothing we can do
+            while updatedMap[boxPosition.y][boxPosition.x] != .wall {
+                defer {
+                    // iterate:
+                    boxPosition = boxPosition.move(in: direction)
+                }
+
+                switch updatedMap[boxPosition.y][boxPosition.x] {
+                case .wall: break
+                case .box: continue // keep going until we find empty space or wall
+                case .empty:
+                    // move the box here..
+                    updatedMap[boxPosition.y][boxPosition.x] = .box
+                    // and the robot to where the box started!
+                    updatedMap[newPosition.y][newPosition.x] = .robot
+                    updatedMap[position.y][position.x] = .empty
+                    return Grid(values: updatedMap)
+                case .robot: preconditionFailure("there should be only one robot!")
+                }
+
+            }
+        }
+
+        return Grid(values: updatedMap)
+    }
+
+    func calculateGPSCoodinates(for grid: Grid<FactoryContents>) -> Int {
+        let boxes = grid.findPositions(of: .box)
+
+        return boxes
+            .map { 100 * $0.y + $0.x }
+            .reduce(0, +)
+    }
+}
+
+enum FactoryContents: Character {
+    case robot = "@"
+    case box = "O"
+    case wall = "#"
+    case empty = "."
+}
+extension FactoryContents: CustomStringConvertible {
+    var description: String { String(rawValue) }
+}
+
+enum Move: Character {
+    case up = "^"
+    case right = ">"
+    case left = "<"
+    case down = "v"
+}
+
+extension Move {
+    var direction: Direction {
+        switch self {
+        case .up: return .north
+        case .right: return .east
+        case .down: return .south
+        case .left: return .west
+        }
     }
 }
--- Tests/AdventOfCodeTests/Day 15/Day15Tests.swift
+++ Tests/AdventOfCodeTests/Day 15/Day15Tests.swift
@@ -5,10 +5,197 @@ final class Day15Tests: XCTestCase, SolutionTest {
     typealias SUT = Day15
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(sut.calculatePartOne(), 2028)
     }
     
     func testPartTwo() throws {
         try XCTAssertEqual(sut.calculatePartTwo(), 0)
+    }
+}
+
+extension Day15Tests {
+    func testParsing() throws {
+        XCTAssertEqual(
+            try sut.map.values.map { $0.map(\.rawValue) },
+            [
+                ["#", "#", "#", "#", "#", "#", "#", "#"],
+                ["#", ".", ".", "O", ".", "O", ".", "#"],
+                ["#", "#", "@", ".", "O", ".", ".", "#"],
+                ["#", ".", ".", ".", "O", ".", ".", "#"],
+                ["#", ".", "#", ".", "O", ".", ".", "#"],
+                ["#", ".", ".", ".", "O", ".", ".", "#"],
+                ["#", ".", ".", ".", ".", ".", ".", "#"],
+                ["#", "#", "#", "#", "#", "#", "#", "#"]
+            ]
+        )
+
+        XCTAssertEqual(try sut.moves, [
+            .left, .up, .up, .right, .right, .right, .down,
+            .down, .left, .down, .right, .right, .down, .left, .left
+        ])
+    }
+
+    func testCalculateGPSCoodinates() throws {
+        let map = Grid(string: """
+        ##########
+        #.O.O.OOO#
+        #........#
+        #OO......#
+        #OO@.....#
+        #O#.....O#
+        #O.....OO#
+        #O.....OO#
+        #OO....OO#
+        ##########
+        """, mapping: FactoryContents.init)
+
+        XCTAssertEqual(try sut.calculateGPSCoodinates(for: map), 10092)
+    }
+
+    func testMakeMoveLeft() throws {
+        let moveOne = try sut.makeMove(.left, grid: sut.map)
+        XCTAssertEqual(try sut.map, moveOne)
+    }
+
+    func testMakeMoveUp() throws {
+        let moveTwo = try sut.makeMove(.up, grid: sut.map)
+        let expectedResult = Grid(
+            string: """
+            ########
+            #.@O.O.#
+            ##..O..#
+            #...O..#
+            #.#.O..#
+            #...O..#
+            #......#
+            ########
+            """,
+            mapping: FactoryContents.init
+        )
+
+        XCTAssertEqual(moveTwo, expectedResult)
+    }
+
+    func testMakeMoveIntoBox() throws {
+        let moveTwo = try sut.makeMove(.right, grid: Grid(string: """
+        ########
+        #.@O.O.#
+        ##..O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########
+        """, mapping: FactoryContents.init))
+
+        let expectedResult = Grid(
+            string: """
+            ########
+            #..@OO.#
+            ##..O..#
+            #...O..#
+            #.#.O..#
+            #...O..#
+            #......#
+            ########
+            """,
+            mapping: FactoryContents.init
+        )
+
+        XCTAssertEqual(moveTwo, expectedResult)
+    }
+
+    func testMakeMoveIntoMultipleBoxes() throws {
+        let moveTwo = try sut.makeMove(.right, grid: Grid(string: """
+        ########
+        #..@OO.#
+        ##..O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########
+        """, mapping: FactoryContents.init))
+
+        let expectedResult = Grid(
+            string: """
+            ########
+            #...@OO#
+            ##..O..#
+            #...O..#
+            #.#.O..#
+            #...O..#
+            #......#
+            ########
+            """,
+            mapping: FactoryContents.init
+        )
+
+        XCTAssertEqual(moveTwo, expectedResult)
+    }
+
+    func testMakeMoveIntoWallWithBoxes() throws {
+        let grid = Grid(string: """
+        ########
+        #...@OO#
+        ##..O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########
+        """, mapping: FactoryContents.init)
+
+        let moveTwo = try sut.makeMove(.right, grid: grid)
+
+        XCTAssertEqual(moveTwo, grid)
+    }
+
+    func testMakeMoves() throws {
+        let finalState = try sut.makeMoves(sut.moves, on: sut.map)
+
+        let expectedResult = Grid(
+            string: """
+            ########
+            #....OO#
+            ##.....#
+            #.....O#
+            #.#O@..#
+            #...O..#
+            #...O..#
+            ########
+            """,
+            mapping: FactoryContents.init
+        )
+
+        XCTAssertEqual(finalState, expectedResult)
+    }
+
+    func testLargerExample() {
+        let sut = SUT(input: """
+        ##########
+        #..O..O.O#
+        #......O.#
+        #.OO..O.O#
+        #..O@..O.#
+        #O#..O...#
+        #O..O..O.#
+        #.OO.O.OO#
+        #....O...#
+        ##########
+
+        <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+        vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+        ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+        <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+        ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+        ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+        >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+        <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+        ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+        v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+        """)
+
+        XCTAssertEqual(sut.calculatePartOne(), 10092)
     }
 }
