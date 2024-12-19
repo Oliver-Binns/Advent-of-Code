b2dfe3fe3a47fc5fb0f77db00c7b078f3faddd6c
Oliver Binns <mail@oliverbinns.co.uk>
2024-12-19 14:40:15 +0000

    feat: add solutions for day 17, part 1

--- Sources/AdventOfCode/Day 17/Day17.swift
+++ Sources/AdventOfCode/Day 17/Day17.swift
@@ -1,15 +1,164 @@
+import Foundation
+
 struct Day17: Solution {
     static let day = 17
-    
-    init(input: String) {
 
+    let initialState: ComputerState
+    let program: [Int]
+
+    init(input: String) {
+        let regex = /Register A: (?<registerA>\d+)\nRegister B: (?<registerB>\d+)\nRegister C: (?<registerC>\d+)\n\nProgram: (?<program>[\d\,]+)\n/
+
+        guard let match = try? regex.firstMatch(in: input),
+              let registerA = Int(match.output.registerA),
+              let registerB = Int(match.output.registerB),
+              let registerC = Int(match.output.registerC) else {
+            preconditionFailure("Non-matching input")
+        }
+
+        initialState = ComputerState(
+            registerA: registerA,
+            registerB: registerB,
+            registerC: registerC
+        )
+
+        program = match.output.program.split(separator: ",")
+            .map(String.init)
+            .compactMap(Int.init)
     }
     
-    func calculatePartOne() -> Int {
-        0
+    func calculatePartOne() -> String {
+        initialState
+            .runProgram(program)
+            .output
+            .map(\.description)
+            .joined(separator: ",")
     }
-    
+
     func calculatePartTwo() -> Int {
         0
     }
+}
+
+struct ComputerState: Equatable {
+    let registerA: Int
+    let registerB: Int
+    let registerC: Int
+
+    let instructionPointer: Int
+
+    let output: [Int]
+
+    init(registerA: Int,
+         registerB: Int,
+         registerC: Int,
+         instructionPointer: Int = 0,
+         output: [Int] = []
+    ) {
+        self.registerA = registerA
+        self.registerB = registerB
+        self.registerC = registerC
+
+        self.instructionPointer = instructionPointer
+
+        self.output = output
+    }
+
+    func comboOperand(_ operand: Int) -> Int {
+        switch operand {
+        case 0...3:
+            return operand
+        case 4:
+            return registerA
+        case 5:
+            return registerB
+        case 6:
+            return registerC
+        default:
+            preconditionFailure("Invalid program operand: \(operand)")
+        }
+    }
+
+    func runInstruction(
+        opcode: Instruction,
+        operand: Int
+    ) -> ComputerState {
+        switch opcode {
+        case .adv:
+            let numerator = registerA
+            let denominator = 2 ^^ comboOperand(operand)
+            return self.updating(registerA: numerator / denominator)
+        case .bdv:
+            let numerator = registerA
+            let denominator = 2 ^^ comboOperand(operand)
+            return self.updating(registerB: numerator / denominator)
+        case .cdv:
+            let numerator = registerA
+            let denominator = 2 ^^ comboOperand(operand)
+            return self.updating(registerC: numerator / denominator)
+        case .bxl:
+            let bitwiseXOR = registerB ^ operand
+            return self.updating(registerB: bitwiseXOR)
+        case .bst:
+            let modulo8 = comboOperand(operand) % 8
+            return self.updating(registerB: modulo8)
+        case .jnz:
+            guard registerA != 0 else { return self.updating() }
+            return self.updating(instructionPointer: operand)
+        case .bxc:
+            let bitwiseXOR = registerB ^ registerC
+            return self.updating(registerB: bitwiseXOR)
+        case .out:
+            let output = comboOperand(operand) % 8
+            return self.updating(output: output)
+        }
+    }
+
+    func runProgram(_ program: [Int]) -> ComputerState {
+        let programLength = program.count
+
+        var state = self
+        while state.instructionPointer < programLength,
+              let opcode = Instruction(rawValue: program[state.instructionPointer]) {
+            state = state.runInstruction(
+                opcode: opcode,
+                operand: program[state.instructionPointer + 1]
+            )
+        }
+
+        return state
+    }
+
+    func updating(
+        registerA: Int? = nil,
+        registerB: Int? = nil,
+        registerC: Int? = nil,
+        instructionPointer: Int? = nil,
+        output: Int? = nil
+    ) -> ComputerState{
+        ComputerState(
+            registerA: registerA ?? self.registerA,
+            registerB: registerB ?? self.registerB,
+            registerC: registerC ?? self.registerC,
+            instructionPointer: instructionPointer ?? (self.instructionPointer + 2),
+            output: self.output + [output].compactMap { $0 }
+        )
+    }
+}
+
+enum Instruction: Int {
+    case adv = 0
+    case bxl = 1
+    case bst = 2
+    case jnz = 3
+    case bxc = 4
+    case out = 5
+    case bdv = 6
+    case cdv = 7
+}
+
+precedencegroup PowerPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ^^ : PowerPrecedence
+func ^^ (radix: Int, power: Int) -> Int {
+    return Int(pow(Double(radix), Double(power)))
 }
--- Tests/AdventOfCodeTests/Day 17/Day17Tests.swift
+++ Tests/AdventOfCodeTests/Day 17/Day17Tests.swift
@@ -5,10 +5,137 @@ final class Day17Tests: XCTestCase, SolutionTest {
     typealias SUT = Day17
     
     func testPartOne() throws {
-        try XCTAssertEqual(sut.calculatePartOne(), 0)
+        try XCTAssertEqual(
+            sut.calculatePartOne(),
+            "4,6,3,5,6,3,5,2,1,0"
+        )
     }
     
     func testPartTwo() throws {
-        try XCTAssertEqual(sut.calculatePartTwo(), 0)
+        try XCTAssertEqual(
+            sut.calculatePartTwo(),
+            0
+        )
+    }
+}
+
+extension Day17Tests {
+    func testParsing() throws {
+        try XCTAssertEqual(sut.initialState.registerA, 729)
+        try XCTAssertEqual(sut.initialState.registerB, 0)
+        try XCTAssertEqual(sut.initialState.registerC, 0)
+
+        try XCTAssertEqual(sut.initialState.instructionPointer, 0)
+        try XCTAssertEqual(sut.initialState.output, [])
+
+        try XCTAssertEqual(sut.program, [0, 1, 5, 4, 3, 0])
+    }
+
+    func testOperandValue() {
+        let computer = ComputerState(
+            registerA: 15,
+            registerB: 30,
+            registerC: 45
+        )
+
+        XCTAssertEqual(computer.comboOperand(0), 0)
+        XCTAssertEqual(computer.comboOperand(1), 1)
+        XCTAssertEqual(computer.comboOperand(2), 2)
+        XCTAssertEqual(computer.comboOperand(3), 3)
+
+        XCTAssertEqual(computer.comboOperand(4), 15)
+        XCTAssertEqual(computer.comboOperand(5), 30)
+        XCTAssertEqual(computer.comboOperand(6), 45)
+    }
+
+    func testRunInstructionAdv() throws {
+        let computer = try sut.initialState
+
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .adv, operand: 2),
+            ComputerState(registerA: 182, registerB: 0, registerC: 0, instructionPointer: 2)
+        )
+    }
+
+    func testRunInstructionBxl() {
+        let computer = ComputerState(registerA: 0, registerB: 2, registerC: 0)
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .bxl, operand: 1),
+            ComputerState(registerA: 0, registerB: 3, registerC: 0, instructionPointer: 2)
+        )
+
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .bxl, operand: 2),
+            ComputerState(registerA: 0, registerB: 0, registerC: 0, instructionPointer: 2)
+        )
+    }
+
+    func testRunInstructionBst() throws {
+        let computer = try sut.initialState
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .bst, operand: 3),
+            ComputerState(registerA: 729, registerB: 3, registerC: 0, instructionPointer: 2)
+        )
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .bst, operand: 4),
+            ComputerState(registerA: 729, registerB: 1, registerC: 0, instructionPointer: 2)
+        )
+    }
+
+    func testRunInstructionJnzNoJump() {
+        let computer = ComputerState(registerA: 0, registerB: 0, registerC: 0)
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .jnz, operand: 4),
+            ComputerState(registerA: 0, registerB: 0, registerC: 0, instructionPointer: 2)
+        )
+    }
+
+    func testRunInstructionJnzWithJump() throws {
+        let computer = try sut.initialState
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .jnz, operand: 4),
+            ComputerState(registerA: 729, registerB: 0, registerC: 0, instructionPointer: 4)
+        )
+    }
+
+    func testRunInstructionBxc() throws {
+        let computer = ComputerState(registerA: 729, registerB: 12, registerC: 5)
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .bxc, operand: 3),
+            ComputerState(registerA: 729, registerB: 9, registerC: 5, instructionPointer: 2)
+        )
+    }
+
+    func testRunInstructionOut() throws {
+        let computer = try sut.initialState
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .out, operand: 0),
+            ComputerState(registerA: 729, registerB: 0, registerC: 0,
+                          instructionPointer: 2, output: [0])
+        )
+
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .out, operand: 4),
+            ComputerState(registerA: 729, registerB: 0, registerC: 0,
+                          instructionPointer: 2, output: [1])
+        )
+    }
+
+    func testRunInstructionBdv() throws {
+        let computer = try sut.initialState
+
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .bdv, operand: 2),
+            ComputerState(registerA: 729, registerB: 182, registerC: 0, instructionPointer: 2)
+        )
+    }
+
+    func testRunInstructionCdv() throws {
+        let computer = try sut.initialState
+
+        XCTAssertEqual(
+            computer.runInstruction(opcode: .cdv, operand: 2),
+            ComputerState(registerA: 729, registerB: 0, registerC: 182, instructionPointer: 2)
+        )
     }
 }
